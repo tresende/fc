c2811339e7d40c043df14170ad2e75ccd93242d5
Thiago Resende <thiago.gcresende@gmail.com>
2023-02-25 13:54:42 -0300

    Add node service

---
+++ docker/mysql/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE people (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+insert into people (name) values('Thiago');
--- docker/node/index.js
+++ docker/node/index.js
@@ -1,11 +1,25 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Thiago')`;
+connection.query(sql);
+connection.end();
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-  console.log("running at localhost:3000");
+  console.log("running at http://localhost:3000");
 });
