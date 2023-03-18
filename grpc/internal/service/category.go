bbeb1e0118693671ea639f4ae69901fafd49dfbc
Thiago Resende <thiago.gcresende@gmail.com>
2023-03-18 19:04:09 -0300

    feat: create GetCategory RPC

--- grpc/internal/service/category.go
+++ grpc/internal/service/category.go
@@ -39,6 +39,21 @@ func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb
 	return &pb.CategoryList{Categories: categoriesResponse}, nil
 }
 
+func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Find(in.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return categoryResponse, nil
+}
+
 func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
 	category, err := c.CategoryDB.Create(in.Name, in.Description)
 	if err != nil {
