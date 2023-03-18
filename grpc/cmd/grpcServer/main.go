7e6a82b665f470d601f762d143f1858d4784aba9
Thiago Resende <thiago.gcresende@gmail.com>
2023-03-18 12:21:45 -0300

    feat: create category procedure call

---
+++ grpc/cmd/grpcServer/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/tresende/fc/internal/database"
+	"github.com/tresende/fc/internal/pb"
+	"github.com/tresende/fc/internal/service"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
+	print("Starting...")
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+	print("End!")
+
+}
--- grpc/go.mod
+++ grpc/go.mod
@@ -4,14 +4,15 @@ go 1.19
 
 require (
 	github.com/google/uuid v1.3.0
-	google.golang.org/grpc v1.53.0
-	google.golang.org/protobuf v1.30.0
+	github.com/mattn/go-sqlite3 v1.14.16
+	google.golang.org/grpc v1.50.1
+	google.golang.org/protobuf v1.28.1
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
-	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
