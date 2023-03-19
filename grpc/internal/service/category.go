0353bf77f36baa5ea053279f4b3707bd00229e74
Thiago Resende <thiago.gcresende@gmail.com>
2023-03-19 14:25:18 -0300

    create stream rpc

--- grpc/internal/service/category.go
+++ grpc/internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/tresende/fc/internal/database"
 	"github.com/tresende/fc/internal/pb"
@@ -67,4 +68,29 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 	}
 
 	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+
+	}
 }
