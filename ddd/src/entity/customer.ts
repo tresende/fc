008cf3f7e0464f2d046c406f42359c7f9aa805a1
Thiago Resende <thiago.gcresende@gmail.com>
2023-05-24 10:06:14 -0300

    sync

--- ddd/src/entity/customer.ts
+++ ddd/src/entity/customer.ts
@@ -1,23 +1,34 @@
 class Customer {
   private readonly _id: string
-  private readonly _name: string
+  private _name: string
   private readonly _address: string
+  private _active = true
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string) {
     this._id = id
+    this.validate()
+  }
+
+  validate() {
+    if (!this._name) {
+      throw new Error('Name is required')
+    }
+    if (!this._id) {
+      throw new Error('Id is required')
+    }
+  }
+
+  changeName(name: string) {
     this._name = name
-    this._address = address
   }
 
-  get id() {
-    return this._id
+  activate() {
+    this._active = true
   }
 
-  get name() {
-    return this._name
-  }
-
-  get address() {
-    return this._address
+  desactivate() {
+    this._active = false
   }
 }
+
+const customer = new Customer('1')
