b495299bce846752f47415dc03623ba44894a324
Thiago Resende <thiago.gcresende@gmail.com>
2024-10-07 13:26:40 -0300

    sync

--- codeflix/catalog-admin/infrastructure/src/test/java/com/tresende/catalog/admin/infrastructure/category/CategoryMySqlGatewayTest.java
+++ codeflix/catalog-admin/infrastructure/src/test/java/com/tresende/catalog/admin/infrastructure/category/CategoryMySqlGatewayTest.java
@@ -1,6 +1,7 @@
 package com.tresende.catalog.admin.infrastructure.category;
 
 import com.tresende.catalog.admin.domain.category.Category;
+import com.tresende.catalog.admin.domain.category.CategoryID;
 import com.tresende.catalog.admin.infrastructure.MySQLGatewayTest;
 import com.tresende.catalog.admin.infrastructure.category.persistence.CategoryJpaEntity;
 import com.tresende.catalog.admin.infrastructure.category.persistence.CategoryRepository;
@@ -58,31 +59,66 @@ public class CategoryMySqlGatewayTest {
         final var expectedIsActive = true;
 
         final var aCategory = Category.newCategory("Film", null, expectedIsActive);
+
         Assertions.assertEquals(0, categoryRepository.count());
 
         categoryRepository.saveAndFlush(CategoryJpaEntity.from(aCategory));
 
-        final var updatedCategory = aCategory.update(expectedName, expectedDescription, expectedIsActive).clone();
-        final var actualCategory = categoryGateway.update(updatedCategory);
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        final var actualInvalidEntity = categoryRepository.findById(aCategory.getId().getValue()).get();
+
+        Assertions.assertEquals("Film", actualInvalidEntity.getName());
+        Assertions.assertNull(actualInvalidEntity.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualInvalidEntity.isActive());
+
+        final var aUpdatedCategory = aCategory.clone()
+                .update(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualCategory = categoryGateway.update(aUpdatedCategory);
 
         Assertions.assertEquals(1, categoryRepository.count());
+
         Assertions.assertEquals(aCategory.getId(), actualCategory.getId());
         Assertions.assertEquals(expectedName, actualCategory.getName());
         Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
         Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
-        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
         Assertions.assertTrue(aCategory.getUpdatedAt().isBefore(actualCategory.getUpdatedAt()));
+        Assertions.assertEquals(aCategory.getDeletedAt(), actualCategory.getDeletedAt());
         Assertions.assertNull(actualCategory.getDeletedAt());
 
         final var actualEntity = categoryRepository.findById(aCategory.getId().getValue()).get();
 
-        Assertions.assertEquals(1, categoryRepository.count());
         Assertions.assertEquals(aCategory.getId().getValue(), actualEntity.getId());
         Assertions.assertEquals(expectedName, actualEntity.getName());
         Assertions.assertEquals(expectedDescription, actualEntity.getDescription());
         Assertions.assertEquals(expectedIsActive, actualEntity.isActive());
-        Assertions.assertTrue(actualEntity.getUpdatedAt().isBefore(aCategory.getUpdatedAt()));
-        Assertions.assertNotNull(actualEntity.getUpdatedAt());
+        Assertions.assertEquals(aCategory.getCreatedAt(), actualEntity.getCreatedAt());
+        Assertions.assertTrue(aCategory.getUpdatedAt().isBefore(actualCategory.getUpdatedAt()));
+        Assertions.assertEquals(aCategory.getDeletedAt(), actualEntity.getDeletedAt());
         Assertions.assertNull(actualEntity.getDeletedAt());
+    }
+
+
+    @Test
+    public void givenAPrePersistedCategoryAndAValidCategoryId_whenTryToDeleteIt_shouldDeleteCategory() {
+        final var aCategory = Category.newCategory("Filmes", "A categoria mais assistida", true);
+        Assertions.assertEquals(0, categoryRepository.count());
+        categoryRepository.saveAndFlush(CategoryJpaEntity.from(aCategory));
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        categoryGateway.deleteById(aCategory.getId());
+
+        Assertions.assertEquals(0, categoryRepository.count());
+    }
+
+    @Test
+    public void givenAPrePersistedCategoryAndAnInValidCategoryId_whenTryToDeleteIt_shouldDeleteCategory() {
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        Assertions.assertDoesNotThrow(() -> categoryGateway.deleteById(CategoryID.from("invalid")));
+
+        Assertions.assertEquals(0, categoryRepository.count());
     }
 }
