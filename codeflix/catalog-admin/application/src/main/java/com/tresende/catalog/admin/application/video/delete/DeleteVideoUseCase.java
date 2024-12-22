9db2977aea8a82fb03451396650a9052f00a0f1a
Thiago Resende <thiago.gcresende@gmail.com>
2024-12-22 19:50:04 -0300

    sync

---
+++ codeflix/catalog-admin/application/src/main/java/com/tresende/catalog/admin/application/video/delete/DeleteVideoUseCase.java
@@ -0,0 +1,6 @@
+package com.tresende.catalog.admin.application.video.delete;
+
+import com.tresende.catalog.admin.application.UnitUseCase;
+
+public abstract class DeleteVideoUseCase extends UnitUseCase<String> {
+}
