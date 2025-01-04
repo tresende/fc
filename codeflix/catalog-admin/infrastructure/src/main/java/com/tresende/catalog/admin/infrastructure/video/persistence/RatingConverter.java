ebf40a4aa90eadbb2cb8eb9ed2deb3ef4250bdaa
Thiago Resende <thiago.gcresende@gmail.com>
2025-01-04 16:16:29 -0300

    sync

---
+++ codeflix/catalog-admin/infrastructure/src/main/java/com/tresende/catalog/admin/infrastructure/video/persistence/RatingConverter.java
@@ -0,0 +1,20 @@
+package com.tresende.catalog.admin.infrastructure.video.persistence;
+
+import com.tresende.catalog.admin.domain.video.Rating;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+class RatingConverter implements AttributeConverter<Rating, String> {
+    @Override
+    public String convertToDatabaseColumn(final Rating attribute) {
+        if (attribute == null) return null;
+        return attribute.getName();
+    }
+
+    @Override
+    public Rating convertToEntityAttribute(final String dbData) {
+        if (dbData == null) return null;
+        return Rating.of(dbData).orElse(null);
+    }
+}
