d23c2adcb8e0029f5a576f658ca3cd6138f7a9d3
Thiago Resende <thiago.gcresende@gmail.com>
2025-02-01 20:03:38 -0300

    sync

--- codeflix/catalog-admin/infrastructure/src/main/java/com/tresende/catalog/admin/infrastructure/Main.java
+++ codeflix/catalog-admin/infrastructure/src/main/java/com/tresende/catalog/admin/infrastructure/Main.java
@@ -1,15 +1,21 @@
 package com.tresende.catalog.admin.infrastructure;
 
 import com.tresende.catalog.admin.infrastructure.configuration.WebServerConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
-
         SpringApplication.run(WebServerConfig.class);
+    }
+
+    @RabbitListener(queues = "video.encoded.queue")
+    void dummyListener() {
+    
     }
 }
--- codeflix/catalog-admin/infrastructure/src/main/java/com/tresende/catalog/admin/infrastructure/configuration/AmqpConfig.java
+++ codeflix/catalog-admin/infrastructure/src/main/java/com/tresende/catalog/admin/infrastructure/configuration/AmqpConfig.java
@@ -1,6 +1,13 @@
 package com.tresende.catalog.admin.infrastructure.configuration;
 
+import com.tresende.catalog.admin.infrastructure.configuration.annotations.VideoCreatedQueue;
+import com.tresende.catalog.admin.infrastructure.configuration.annotations.VideoEncodedQueue;
+import com.tresende.catalog.admin.infrastructure.configuration.annotations.VideoEvents;
 import com.tresende.catalog.admin.infrastructure.configuration.properties.amqp.QueueProperties;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +17,63 @@ public class AmqpConfig {
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-created")
-    public QueueProperties videoCreatedQueue() {
+    @VideoCreatedQueue
+    QueueProperties videoCreatedQueueProperties() {
         return new QueueProperties();
     }
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-encoded")
-    public QueueProperties videoEncodedQueue() {
+    @VideoEncodedQueue
+    QueueProperties videoEncodedQueueProperties() {
         return new QueueProperties();
+    }
+
+    @Configuration
+    static class Admin {
+
+        @Bean
+        @VideoEvents
+        public DirectExchange videoEventsExchange(@VideoCreatedQueue QueueProperties props) {
+            return new DirectExchange(props.getExchange());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Queue videoCreatedQueue(@VideoCreatedQueue QueueProperties props) {
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        public Queue videoEncodedQueue(@VideoEncodedQueue QueueProperties props) {
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Binding videoCreatedBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoCreatedQueue Queue queue,
+                @VideoCreatedQueue QueueProperties props
+        ) {
+            return BindingBuilder
+                    .bind(queue)
+                    .to(exchange)
+                    .with(props.getRoutingKey());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        public Binding videoEncodedBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoEncodedQueue Queue queue,
+                @VideoEncodedQueue QueueProperties props
+        ) {
+            return BindingBuilder
+                    .bind(queue)
+                    .to(exchange)
+                    .with(props.getRoutingKey());
+        }
     }
 }
