42b47b3cb740e9318331b11846a62850c3b75933
Thiago Resende <thiago.gcresende@gmail.com>
2025-06-01 16:45:16 -0300

    Add Kafka Connect service to Docker Compose; create necessary networks and volumes

--- codeflix/catalog-videos/sandbox/run.sh
+++ codeflix/catalog-videos/sandbox/run.sh
@@ -1,8 +1,11 @@
 # Criar as docker networks
 docker network create elastic
+docker network create kafka
 
 #criar docker volumes
 docker volume create es01
+docker volume create kafka01
 
 
 docker compose -f elk/docker-compose.yml up -d elasticsearch
+docker compose -f kafka/docker-compose.yml up -d
