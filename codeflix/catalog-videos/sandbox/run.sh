3fada708ada5cd877e89c85180e1be62c041bc84
Thiago Resende <thiago.gcresende@gmail.com>
2025-06-01 18:23:30 -0300

    Add MySQL and RabbitMQ services to Docker Compose; configure Kafka Connect with necessary settings

--- codeflix/catalog-videos/sandbox/run.sh
+++ codeflix/catalog-videos/sandbox/run.sh
@@ -5,7 +5,8 @@ docker network create kafka
 #criar docker volumes
 docker volume create es01
 docker volume create kafka01
-
+docker volume create kconnect01
 
 docker compose -f elk/docker-compose.yml up -d elasticsearch
 docker compose -f kafka/docker-compose.yml up -d
+docker compose -f services/docker-compose.yml up -d
