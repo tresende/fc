0bffd8de8cd7b5c6a9fad0e4dcf67d8c1fac315f
Thiago Resende <thiago.gcresende@gmail.com>
2025-07-09 13:24:47 -0300

    connfigure Kafka with KRaft mode and add category listener

---
+++ codeflix/catalog-videos/infrastructure/src/main/java/com/tresende/catalog/infrastructure/kafka/CategoryListener.java
@@ -0,0 +1,40 @@
+package com.tresende.catalog.infrastructure.kafka;
+
+import com.tresende.catalog.application.category.delete.DeleteCategoryUseCase;
+import com.tresende.catalog.application.category.save.SaveCategoryUseCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class CategoryListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryListener.class.getName());
+
+    private final SaveCategoryUseCase saveCategoryUseCase;
+    private final DeleteCategoryUseCase deleteCategoryUseCase;
+
+    public CategoryListener(final SaveCategoryUseCase saveCategoryUseCase, final DeleteCategoryUseCase deleteCategoryUseCase) {
+        this.saveCategoryUseCase = Objects.requireNonNull(saveCategoryUseCase);
+        this.deleteCategoryUseCase = Objects.requireNonNull(deleteCategoryUseCase);
+    }
+
+    @KafkaListener(
+            concurrency = "${kafka.consumer.categories.concurrency}",
+            topics = "${kafka.consumer.categories.topics}",
+            containerFactory = "containerFactory",
+            groupId = "${kafka.consumer.categories.group-id}",
+            id = "${kafka.consumer.categories.id}",
+            properties = {
+                    "auto.offset.reset=${kafka.consumer.categories.auto-offset-reset}",
+            }
+    )
+    public void handler(@Payload final String message, final ConsumerRecordMetadata metadata) {
+        LOG.info("Received message [topic:{}] [partition:{}]  [offset:{}] {} ", metadata.topic(), metadata.partition(), metadata.offset(), message);
+    }
+}
