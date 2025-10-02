9c6a6678f0dfdbc4232ddb131150273df8e39246
Thiago Resende <thiago.gcresende@gmail.com>
2025-10-02 15:03:07 -0300

    feat(cache): Configure cache TTL and max entries

--- codeflix/catalog-videos/infrastructure/src/main/java/com/tresende/catalog/infrastructure/configuration/CacheConfig.java
+++ codeflix/catalog-videos/infrastructure/src/main/java/com/tresende/catalog/infrastructure/configuration/CacheConfig.java
@@ -1,18 +1,25 @@
 package com.tresende.catalog.infrastructure.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.Cache2kBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration(proxyBeanMethods = false)
 @EnableCaching
 public class CacheConfig {
 
     @Bean
-    Cache2kBuilderCustomizer cache2kCustomizer() {
+    Cache2kBuilderCustomizer cache2kCustomizer(
+            @Value("${cache.ttl:60}") final int ttl,
+            @Value("${cache.max-entries:200}") final int maxEntries
+    ) {
         return builder -> builder
-                .entryCapacity(200)
+                .entryCapacity(maxEntries)
+                .expireAfterWrite(ttl, TimeUnit.SECONDS)
                 .build();
     }
 }
